69a792fb4eae287d764b637177acef2b021f195f
will127534 <will3509111@gmail.com>
2013-12-15 00:24:19 +0800

    Update v1.1
    Adding python code for cosm update & miminize Arduino code for remote
    station

--- remoteststion/station/station.ino
+++ remoteststion/station/station.ino
@@ -2,30 +2,19 @@
 #include <avr/power.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <math.h> 
 #include <DHT22.h>
 #include <BMP085.h>
 #include <nRF24L01.h>
 #include <Mirf.h>
 #include "nRF24L01.h"
-#include <RF24.h>
-#include <MirfHardwareSpiDriver.h>
 #include <util/atomic.h>
+
 BMP085 dps = BMP085();
  ISR(WDT_vect) { watchdogEvent(); }
-int serialValue;
 DHT22 myDHT22(9);
-const unsigned char OSS = 0;  // Oversampling Setting
 
-#define DS00OV	0x80
-#define DS00UV	0x40
-#define DS00COC	0x20
-#define DS00DOC	0x10
-#define DS00CC	0x08
-#define DS00DC	0x04
-#define DS00CE	0x02
-#define DS00DE	0x01
-
+const unsigned char OSS = 3;  // Oversampling Setting
+int serialValue;
 int DHTPOEWR=8;
 int dsAddress = 0x34;
 int reading = 0; 
@@ -39,23 +28,22 @@ float h;
 int csn_pin = 7;
 
 struct config_type
- {                     // 16 bytes to send to the Server (on 4 byte boundries)
-float humidity;
-float celsius;
-int finalTempC;
-float vo;
-uint16_t lux;
-int32_t pressure;
-float mah;
-float kPressure;
-   
-            
-} ;
- config_type data;
+    {                     
+      float humidity;
+      float celsius;
+      int finalTempC;
+      float vo;
+      uint16_t lux;
+      int32_t pressure;
+      float mah;
+      float kPressure;    
+    };
+config_type data;
+
 void setup(){ 
-pinMode(DHTPOEWR, OUTPUT); 
-pinMode(13, OUTPUT); 
-digitalWrite(DHTPOEWR,HIGH);
+  pinMode(DHTPOEWR, OUTPUT); 
+  pinMode(13, OUTPUT); 
+  digitalWrite(DHTPOEWR,HIGH);
   Mirf.csnPin = 6;
   Mirf.cePin = 10;
   Mirf.spi = &MirfHardwareSpi;
@@ -81,21 +69,21 @@ digitalWrite(DHTPOEWR,HIGH);
 
 void loop(){
       digitalWrite(DHTPOEWR,HIGH);
-      readDHT2(); //2104
+      readDHT2(); 
       digitalWrite(DHTPOEWR,LOW);
       dps.getPressure(&data.pressure);
-  //    Serial.println(data.pressure);
-       data.vo = getdsVoltage();
-       data.mah = getdsmah();
-      // Serial.println(data.pressure);
-      // Serial.println(data.celsius);
-     //  Serial.println(data.vo);
+      //Serial.println(data.pressure);
+      data.vo = getdsVoltage();
+      data.mah = getdsmah();
+      //Serial.println(data.pressure);
+      //Serial.println(data.celsius);
+      //Serial.println(data.vo);
       digitalWrite(13, HIGH);
       sendnrf();
       digitalWrite(13, LOW);
-       loseSomeTime (100);
-  
-      powerDown2();
+      //loseSomeTime (100);
+
+      Mirf.powerDown();
       loseSomeTime (30000);
      
 }
@@ -176,7 +164,7 @@ void watchdogEvent() {
 
 
 void sendnrf(){
-   Mirf.setTADDR((byte *)"serv1");
+  Mirf.setTADDR((byte *)"serv1");
   
   Mirf.send((byte *)&data);
   
@@ -184,6 +172,7 @@ void sendnrf(){
   }
   Serial.println("Finished sending");
   }
+
 void readDHT2(){
   DHT22_ERROR_t errorCode;
   loseSomeTime (2100);
@@ -196,58 +185,8 @@ void readDHT2(){
       //Serial.println(data.humidity);
   }
 }
-void powerDown2()
-{
-  write_register(CONFIG,read_register(CONFIG) & ~_BV(PWR_UP));
-}
-
-uint8_t write_register(uint8_t reg, uint8_t value)
-{
-uint8_t status;
-csn(LOW);
-status = SPI.transfer( W_REGISTER | ( REGISTER_MASK & reg ) );
-SPI.transfer(value);
-csn(HIGH);
-return status;
-}
-
-uint8_t read_register(uint8_t reg)
-{
-  csn(LOW);
-  SPI.transfer( R_REGISTER | ( REGISTER_MASK & reg ) );
-  uint8_t result = SPI.transfer(0xff);
-
-  csn(HIGH);
-  return result;
-}
-
-void csn(int mode)
-{
-  // Minimum ideal SPI bus speed is 2x data rate
-  // If we assume 2Mbs data rate and 16Mhz clock, a
-  // divider of 4 is the minimum we want.
-  // CLK:BUS 8Mhz:2Mhz, 16Mhz:4Mhz, or 20Mhz:5Mhz
-#ifdef ARDUINO
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV4);
-#endif
-  digitalWrite(csn_pin,mode);
-}
 
 
-double dewPoint(double celsius, double humidity)
-{
-        double A0= 373.15/(273.15 + celsius);
-        double SUM = -7.90298 * (A0-1);
-        SUM += 5.02808 * log10(A0);
-        SUM += -1.3816e-7 * (pow(10, (11.344*(1-1/A0)))-1) ;
-        SUM += 8.1328e-3 * (pow(10,(-3.49149*(A0-1)))-1) ;
-        SUM += log10(1013.246);
-        double VP = pow(10, SUM-3) * humidity;
-        double T = log(VP/0.61078);   // temp var
-        return (241.88 * T) / (17.558-T);
-}
 
 void temp()
 {
@@ -327,38 +266,9 @@ float getdsTemp() {
 	return reading;
 }
 
-int getdsProtection(void) {
-	int dsProtect;
-	int dsStatus;
-	// Read Protection Register
-	Wire.beginTransmission(dsAddress);
-	Wire.write(0x00);
-	Wire.endTransmission();
-	Wire.requestFrom(dsAddress, 2);
-	if(2 <= Wire.available())     // if two bytes were received 
-	{ 
-		dsProtect = Wire.read();
-		dsStatus = Wire.read();
-		//Serial.println(dsProtect,BIN);
-		//Serial.println(dsStatus,BIN);
-
-		if (dsProtect & DS00OV) { Serial.print("Over Voltage, "); }
-		if (dsProtect & DS00UV) { Serial.print("Under Voltage, "); }
-		if (dsProtect & DS00COC) { Serial.print("Charge Over Current, "); }
-		if (dsProtect & DS00DOC) { Serial.print("Discharge Over Current, "); }
-		if (dsProtect & DS00CC) { Serial.print("CC Pin state, "); }
-		if (dsProtect & DS00DC) { Serial.print("DC Pin State, "); }
-		if (dsProtect & DS00CE) { Serial.print("Charging Enabled, "); }
-		if (dsProtect & DS00DE) { Serial.print("Discharging Enabled, "); }
-		if (dsStatus & 32) { Serial.print("Sleep mode enabled, ");}
-	}
-	return dsProtect;
-}
-
 float getdsVoltage(void) {
 	int voltage;
-        
-	
+
 	// Read Voltage Register
 	Wire.beginTransmission(dsAddress);
 	Wire.write(0x0C);
@@ -372,12 +282,11 @@ float getdsVoltage(void) {
 		voltage = voltage << 8;
 		voltage += Wire.read();
 		voltage = voltage >> 5;
-	        v = voltage * 4.88 / 1000;
-		
-		
+	  v = voltage * 4.88 / 1000;
 	}
 	return v;
 }
+
 float getdsCurrent(void) {
 	int current;
 	
@@ -454,50 +363,6 @@ void resetds() {
 	Wire.endTransmission(); 	
 }
 
-void showall(void) { 
-  Serial.println("Current BMP085 settings");
-  Serial.println("==========================================================");
-  Serial.print("device address                  = 0x");
-  Serial.println(dps.getDevAddr(), HEX);
-  Serial.print("Mode                            = ");
-  switch (dps.getMode()) {
-    case MODE_ULTRA_LOW_POWER: 
-      Serial.println("MODE_ULTRA_LOW_POWER");
-      break;
-    case MODE_STANDARD: 
-      Serial.println("MODE_STANDARD");
-      break;    
-    case MODE_HIGHRES: 
-      Serial.println("MODE_HIGHRES");
-      break;    
-    case MODE_ULTRA_HIGHRES:     
-      Serial.println("MODE_ULTRA_HIGHRES");
-      break; 
-  }  
-}
-
-void dumpRegisters() {
-  byte ValidRegisterAddr[]={0xAA,0xAB,0xAC,0xAD,0xAE,0xAF,0xB0,0xB1,0xB2,0xB3,0xB4,0xB5,0xB6,0xB7,0xB8,0xB9,0xBA,0xBB,0xBC,0xBD,0xBE,0xBF,0xF6,0xF7,0xF8,0xF9}; 
-  byte _b, i, totregisters = sizeof(ValidRegisterAddr);
-  Serial.println("---dump start---");
-  Serial.println("Register address|Register data");
-  Serial.println("Reg.address(hex,dec) Reg.data(bin,hex,dec)");
-  for (i=0;i<totregisters;i++){    
-    Serial.print("0x");
-    Serial.print(ValidRegisterAddr[i], HEX);
-    Serial.print(",");
-    Serial.print(ValidRegisterAddr[i], DEC);
-    Serial.print(",");
-    dps.readmem(ValidRegisterAddr[i], 1, &_b);
-    Serial.print("b");
-    print_bits(_b);
-    Serial.print(",0x");
-    Serial.print(_b,HEX);
-    Serial.print(",");
-    Serial.println(_b,DEC);  
-  }
-  Serial.println("---dump end---");
-}
 
 void print_bits(byte val){
   int i;
